4cd45aa5954fc176a56dd92d8b660b3e459f0ff6
sandrofonseca <sfonseca@Sandro-Fonseca-de-Souza-macbook-pro.local>
2014-09-30 19:29:07 -0300

    Including pompty files

---
+++ MC_config/pompty/config_pompty_TOTEM/DY_MUMU_M_10_20_pompyt_plus_cff_py_GEN_SIM.py
@@ -0,0 +1,141 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.14 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: GeneratorInterface/PompytInterface/python/pompyt_cff.py --step GEN,SIM --beamspot Realistic8TeVCollision --conditions START52_V9::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.372.2.14 $'),
+    annotation = cms.untracked.string('GeneratorInterface/PompytInterface/python/pompyt_cff.py nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('DY_MUMU_M_10_20_pompyt_plus_cff_py_GEN_SIM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+process.generator = cms.EDProducer("PompytProducer",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    pdiss_B0 = cms.untracked.double(1.5),
+    pomZ = cms.untracked.double(1.0),
+    pomPdiss = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model', 
+            'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'PARP(82)=1.8387   ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1960. ! sqrts for which PARP82 is set', 
+            'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter', 
+            'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter', 
+            'PARP(90)=0.16  ! Multiple interactions: rescaling power', 
+            'PARP(67)=2.5    ! amount of initial-state radiation', 
+            'PARP(85)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(86)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(62)=1.25   ! ', 
+            'PARP(64)=0.2    ! ', 
+            'MSTP(91)=1      !', 
+            'PARP(91)=2.1   ! kt distribution', 
+            'PARP(93)=15.0  ! '),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'pytParameters'),
+	pytParameters = cms.vstring('MSEL=0',
+            'MSUB(1)=1         !Incl Z0/gamma* production',
+            'MSTP(43)=3',
+            'MDME(174,1)=0     !Z decay into d dbar',
+            'MDME(175,1)=0     !Z decay into u ubar',
+            'MDME(176,1)=0     !Z decay into s sbar',
+            'MDME(177,1)=0     !Z decay into c cbar',
+            'MDME(178,1)=0     !Z decay into b bbar',
+            'MDME(179,1)=0     !Z decay into t tbar',
+            'MDME(182,1)=0     !Z decay into e- e+',
+            'MDME(183,1)=0     !Z decay into nu_e nu_ebar',
+            'MDME(184,1)=1     !Z decay into mu- mu+',
+            'MDME(185,1)=0     !Z decay into nu_mu nu_mubar',
+            'MDME(186,1)=0     !Z decay into tau- tau+',
+            'MDME(187,1)=0     !Z decay into nu_tau nu_taubar',
+	    'CKIN(1)=10.       !Minimum sqrt(s_hat) value (=Z mass)',
+            'CKIN(2)=20.       !Maximum sqrt(s_hat) value (=Z mass)'),	    
+  ),
+    pdiss_MMAX = cms.untracked.double(2000.0),
+    comEnergy = cms.untracked.double(8000.0),
+    pdiss_GLU = cms.untracked.double(0.5),
+    maxEventsToPrint = cms.untracked.int32(1),
+    xiMax = cms.untracked.double(0.2),
+    pdiss_AP = cms.untracked.double(0.016)
+)
+
+
+
+
+
+
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
